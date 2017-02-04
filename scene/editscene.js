dd9be4e38e4575fade91e98aad87fb827f68831d
nondefault <spam@nondefault.net>
2017-02-03 21:01:15 -0600

    Grid-aligned placement of obstacles
    Editor UI has been improved a bit.  Framework is ready for placement of arbitrary obstacles.

--- scene/editscene.js
+++ scene/editscene.js
@@ -1,6 +1,9 @@
 var EditScene = {
     stage: null,
     selectedObstacle: null,
+    phantomObstacle: null,
+    allowPlayerFire: false,
+    gridScale: 8,
 
     init: function(params) {
         EditScene.stage = new PIXI.Container();
@@ -11,18 +14,63 @@ var EditScene = {
         GameScene.world.listen("addObstacle", this.handleAddObstacle);
         GameScene.world.obstacles.forEach(this.handleAddObstacle);
 
+        //Hijack GameScene stage
         EditScene.stage.addChild(GameScene.stage);
         GameScene.stage.interactive = true;
+
+        //Override player fire action to prevent it
+        var playerFire = GameScene.world.player.fire;
+        GameScene.world.player.fire = function(){
+            if (EditScene.allowPlayerFire)
+                playerFire.apply(GameScene.world.player, arguments);
+        };
+
+        EditScene.initUI();
+
+        //Set up editor interaction
         GameScene.stage.on("click", function(){
-            GameScene.world.addObstacle(new Obstacle({
-                vertices: [new Vector(-8,-8), new Vector(8,-8), new Vector(8,8), new Vector(-8,8)],
-                position: new Vector(Input.mouse).add(GameScene.view).add(GameScene.viewOffset)
-            }));
+            if (EditScene.selectedObstacle !== null)
+                GameScene.world.buildObstacle(
+                    EditScene.selectedObstacle,
+                    EditScene.calculatePlaceLocation(new Vector(Input.mouse)).add(GameScene.viewOffset)
+                );
         });
 
+        EditScene.selectObstacle({
+            type: "obstacle",
+            vertices: [new Vector(0,0), new Vector(16,0), new Vector(16,16), new Vector(0,16)]
+        });
+
+        Game.WALLHACKS = true;
+        GameScene.LOOK_INTENSITY = 0;
+    },
+
+    selectObstacle(data) {
+        EditScene.selectedObstacle = data;
+        EditScene.phantomObstacle = new Obstacle(Object.assign({position: new Vector(0,0)}, data));
+        EditScene.phantomObstacle.draw();
+        EditScene.phantomObstacle.gfx.alpha = 0.75;
+        EditScene.phantomObstacle.gfx.hitArea = new PIXI.Rectangle(0,0,0,0);
+
+        //remove old phantom object graphics if any
+        var idx = EditScene.stage.children.indexOf(EditScene.phantomObstacle.gfx);
+        if (idx >= 0) {
+            EditScene.stage.removeChild(EditScene.phantomObstacle.gfx);
+        }
+
+        //add new phantom object graphics
+        idx = EditScene.stage.children.indexOf(EditScene.overlayGfx);
+        EditScene.stage.addChildAt(EditScene.phantomObstacle.gfx, idx+1);
+    },
+
+    initUI: function() {
+        //create graphics overlay
+        EditScene.overlayGfx = new PIXI.Graphics();
+
+        //create debug text
         EditScene.debugText = new PIXI.extras.BitmapText("debug: ", {
             font: "AndinaBold",
-			tint: 0xFFFFFF
+            tint: 0xFFFFFF
         });
         EditScene.debugText.position = new PIXI.Point(8,8);
         
@@ -34,12 +82,67 @@ var EditScene = {
         dbtDisplayShadow.position.y = 1;
         dbtDisplayShadow.tint = Core.color.bg2;
 
+        EditScene.stage.addChild(EditScene.overlayGfx);
         EditScene.stage.addChild(dbtDisplayShadow);
         EditScene.stage.addChild(dbtDisplayFore);
-        Game.WALLHACKS = true;
-        GameScene.LOOK_INTENSITY = 0;
     },
 
+    /**
+     * Updates the contents of the graphical overlay, including the placement
+     * grid and phantom object.
+     */
+    updateOverlay: function() {
+        var gfx = EditScene.overlayGfx;
+        gfx.clear();
+        var pos = EditScene.calculatePlaceLocation(new Vector(Input.mouse));
+        pos = pos.sub(GameScene.view);
+
+        //placement grid calculations
+        const GRID_RANGE_PX = 64;
+        var steps = Math.ceil(GRID_RANGE_PX / EditScene.gridScale);
+        var x0 = pos.x - GRID_RANGE_PX;
+        var x1 = pos.x + GRID_RANGE_PX;
+        var y0 = pos.y - GRID_RANGE_PX;
+        var y1 = pos.y + GRID_RANGE_PX;
+        var opacity = z => (1-Math.abs(z/steps))*0.25;
+
+        //draw placement grid
+        for (let y=-steps; y<=steps; y++) { 
+            gfx.lineStyle(1, 0x900000, opacity(y));
+            gfx.moveTo(x0, pos.y+y*EditScene.gridScale);
+            gfx.lineTo(x1, pos.y+y*EditScene.gridScale);
+        }
+        for (let x=-steps; x<=steps; x++) { 
+            gfx.lineStyle(1, 0x900000, opacity(x));
+            gfx.moveTo(pos.x+x*EditScene.gridScale, y0);
+            gfx.lineTo(pos.x+x*EditScene.gridScale, y1);
+        }
+
+        //move phantom object
+        if (EditScene.phantomObstacle !== null) {
+            EditScene.phantomObstacle.gfx.position = pos;
+        }
+    },
+
+    /**
+     * Accepts a screen-relative mouse position.
+     * Returns a position representing the world-relative position where an
+     * object should be placed, aligning it to the placement grid.
+     * @param mouse screen-relative mouse Vector (i.e. Input.mouse)
+     * @return world-relative placement Vector
+     */
+    calculatePlaceLocation: function(mouse) {
+        mouse = mouse.add(GameScene.view).div(EditScene.gridScale);
+        mouse.x = Math.floor(mouse.x);
+        mouse.y = Math.floor(mouse.y);
+        mouse = mouse.mult(EditScene.gridScale);
+        return mouse;
+    },
+
+    /**
+     * An event handler for the creation of an obstacle.
+     * Attaches necessary event handlers to the obstacle.
+     */
     handleAddObstacle: function(obs) {
         obs.gfx.on("mouseover", function(){
             obs.gfx.tint = 0xFF0000;
@@ -62,6 +165,7 @@ var EditScene = {
 
     frame: function(timescale, ticks) {
         GameScene.frame(timescale, ticks);
+        EditScene.updateOverlay();
         
         //update debug text
         EditScene.debugText.text = "Editor: " + GameScene.world.levelName;
